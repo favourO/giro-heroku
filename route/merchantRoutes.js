7a14a2bc683ad133785a15f164978b40975cfc38
Favour Ojiaku <ojiakufavour@gmail.com>
2021-07-17 13:57:30 +0100

    merchants added

--- route/merchantRoutes.js
+++ route/merchantRoutes.js
@@ -1,9 +1,11 @@
 const { createMerchant, getMerchants } = require('../controller/merchant');
 const express = require('express');
 const { protect } = require('../middleware/auth');
+const Merchants = require('../model/Merchants');
+const advancedResults = require('../middleware/advancedResults');
 const router = express.Router();
 
 
 router.route('/:userid/create-merchant').post(protect, createMerchant);
-router.route('/').get(getMerchants);
+router.route('/').get(advancedResults(Merchants, 'products'), getMerchants);
 module.exports = router;
