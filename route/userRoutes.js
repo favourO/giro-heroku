6935cfde7c567b17b0561ed543ba95e95c749e4b
Favour Ojiaku <ojiakufavour@gmail.com>
2021-09-19 19:08:49 +0100

    other authentication details

--- route/userRoutes.js
+++ route/userRoutes.js
@@ -1,4 +1,4 @@
-const { createAccount, verify, login, getMe } = require('../controller/auth');
+const { createAccount, verify, login, getMe, logout, updateDetails, updatePassword } = require('../controller/auth');
 const express = require('express');
 
 const { protect } = require('../middleware/auth')
@@ -8,9 +8,10 @@ router
     .post('/register', createAccount)
     .post('/verify', verify)
     .post('/login', login)
-    .get('/me', protect, getMe);
-
-
+    .get('/me', protect, getMe)
+    .get('/logout', logout)
+    .post('/updatedetails', protect, updateDetails)
+    .post('/updatepassword', protect, updatePassword);
 
 
 module.exports = router;
