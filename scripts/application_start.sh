018204166818060af12152b3e66e75e572d076c2
Favour Ojiaku <ojiakufavour@gmail.com>
2021-11-25 05:07:06 +0100

    aws deployment edited

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -7,7 +7,8 @@ cd /home/ec2-user/giro-payment-api
 
 #add npm and node to path
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/bash_completion" # loads nvm bash_completion (node is in )
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # loads nvm bash_completion (node is in )
 
 #install node modules
 npm install
